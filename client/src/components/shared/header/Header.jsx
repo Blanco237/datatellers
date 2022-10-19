3663f3b5f9944a98d7d109b063916b61fee86927
Blanco237 <asongrandy9@gmail.com>
2022-10-19 12:22:23 +0100

    Completed Header

---
+++ client/src/components/shared/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.body}>
+        <section className={styles.headline}>
+            DrNG | PATIENTS
+        </section>
+    </header>
+  )
+}
+
+export default Header
