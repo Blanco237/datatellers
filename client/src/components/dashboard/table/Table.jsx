64dba59ae6c6ef1287965b4cd5241d8014048f56
Blanco237 <asongrandy9@gmail.com>
2022-10-19 19:38:22 +0100

    Completed Table Component

---
+++ client/src/components/dashboard/table/Table.jsx
@@ -0,0 +1,216 @@
+import React, { useState, useRef } from "react";
+import { Table as T, Button, Input, Space } from "antd";
+
+import { SearchOutlined } from "@ant-design/icons";
+
+import "./table.css";
+import styles from "./table.module.css";
+
+import dataSource, { columns } from "./data";
+
+const Table = () => {
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+          size="small"
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1890ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) => (searchedColumn === dataIndex ? <p>{text}</p> : text),
+  });
+
+  const scroll = (pagination, filters, sorter, extra) => {
+    if (extra.action === "paginate") {
+      window.scrollTo({
+        top: 150,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      ...getColumnSearchProps("name"),
+      sorter: (a, b) => (a.name > b.name ? 1 : -1),
+    },
+    {
+      title: "Code",
+      dataIndex: "code",
+      key: "code",
+      ...getColumnSearchProps("code"),
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+      filters: [
+        { text: "Male", value: "male" },
+        { text: "Female", value: "female" },
+      ],
+      onFilter: (value, record) => record.gender === value,
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+      key: "phone",
+      ...getColumnSearchProps("phone"),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      filters: [
+        { text: "Pending", value: "pending" },
+        { text: "Missed", value: "missed" },
+        { text: "Passed", value: "passed" },
+        { text: "Rescheduled", value: "rescheduled" },
+      ],
+      onFilter: (value, record) => record.status === value,
+      render: (status) => {
+        const statusObj = {
+          missed: "danger",
+          passed: "success",
+          rescheduled: "warning",
+          pending: "warning",
+        };
+
+        const theme = statusObj[status];
+
+        const styles = {
+          background: `var(--${theme}-accent)`,
+          color: `var(--${theme})`,
+          padding: "0.1rem 0.15rem",
+          fontWeight: "500",
+          fontSize: "0.5rem",
+          borderRadius: "7px",
+          textAlign: "center",
+        };
+
+        return <p style={styles}>{status}</p>;
+      },
+    },
+    { title: "Appointment Date", dataIndex: "appt", key: "appt", sorter: (a, b) =>  new Date(b.appt) - new Date(a.appt), },
+    { title: "Record Date", dataIndex: "record", key: "record", sorter: (a, b) =>  new Date(b.record) - new Date(a.record), },
+  ];
+
+  return (
+    <section className={styles.body}>
+      <T
+        onRow={(record, rowIndex) => {
+          return {
+            onClick: (event) => {
+              console.log(record);
+              console.log(rowIndex);
+            }, // click row
+          };
+        }}
+        onChange={scroll}
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{
+          position: ["bottomCenter"],
+          size: "small",
+          showSizeChanger: false,
+          showTotal: (total) => `${total} Patients`,
+          hideOnSinglePage: true
+        }}
+      />
+    </section>
+  );
+};
+
+export default Table;
