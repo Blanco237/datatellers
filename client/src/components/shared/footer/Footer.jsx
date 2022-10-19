1b11aa8ce55ae134196c84c62d2663319f041e67
Blanco237 <asongrandy9@gmail.com>
2022-10-19 12:22:09 +0100

    Completed Footer

---
+++ client/src/components/shared/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.body}>
+      <p>
+        Built By{" "}
+        <a href="https://github.com/Blanco237" rel="noreferrer" target="_blank" className={styles.link}>
+          Blanco237
+        </a>{" "}
+        for{" "}
+        <a
+          href="https://www.datatellers.info/"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.link}
+        >
+          DataTeller
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
