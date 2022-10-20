3d1c35c3f26a9c87221793be89b7c9c1d2a74e13
Blanco237 <asongrandy9@gmail.com>
2022-10-20 13:56:07 +0100

    Dashboard UI complete

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -6,15 +6,18 @@ import styles from './dash.module.css';
 import Statistics from '../../components/dashboard/statistics/Statistics'
 import Table from '../../components/dashboard/table/Table'
 import { PlusOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
 
 const Dashboard = () => {
   return (
     <main className={styles.body}>
       <Statistics />
       <Table />
+      <Tooltip title={<span>Add New Record</span>}>
       <button className={styles.addButton}>
         <PlusOutlined />
       </button>
+      </Tooltip>
     </main>
   )
 }
