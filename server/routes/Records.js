4c384b4cd54de47d1f018ab46b653fad9326fefc
Blanco237 <asongrandy9@gmail.com>
2022-10-20 21:49:08 +0100

    Added Routes

--- server/routes/Records.js
+++ server/routes/Records.js
@@ -10,21 +10,24 @@ const pad = (n) => {
     return n;
 }
 
-const getCode = (code) => {
+const getCode = async () => {
+    const count = await Records.count();
+    const code = pad(count + 1);
     const d = new Date();
     const day = pad(d.getDate());
     const month = pad(d.getMonth() + 1);
     const year = d.getUTCFullYear().toString().slice(2, 4);
 
-    const uniqueCode = `${code}${day}${month}${year}`;
+    const uniqueCode = `A${code}${day}${month}${year}`;
 
     return uniqueCode;
 }
 
 router.post('/save', async (req, res) => {
     const data = req.body;
-    data.code = getCode(data.code);
-    data.firstTime = firstTime.toLowerCase() === "yes" ? true : false;
+    data.code = await getCode();
+    console.log(data.code);
+    data.firstTime = data.firstTime.toLowerCase() === "yes" ? true : false;
 
     try {
         const record = await Records.create(data);
@@ -33,4 +36,28 @@ router.post('/save', async (req, res) => {
     catch (e) {
         res.status(400).json({ error: e.message });
     }
+});
+
+router.get('/all', async (req, res) => {
+    try {
+        const records = await Records.findAll();
+        res.json(records);
+    }
+    catch (e) {
+        res.status(400).json({ error: e.message });
+    }
+});
+
+router.get('/byCode/:code', async (req, res) => {
+    const { code } = req.params;
+    try {
+        const record = await Records.findByPk(code);
+        res.json(record);
+    }
+    catch (e) {
+        res.status(400).json({ error: e.message })
+    }
 })
+
+
+module.exports = router;
