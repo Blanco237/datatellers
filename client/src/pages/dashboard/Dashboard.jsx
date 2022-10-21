36b90659afd7b15e7cffb73f08d557a01b294b22
Blanco237 <asongrandy9@gmail.com>
2022-10-21 09:08:32 +0100

    Setup Guard and Routing

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -7,14 +7,24 @@ import Statistics from '../../components/dashboard/statistics/Statistics'
 import Table from '../../components/dashboard/table/Table'
 import { PlusOutlined } from '@ant-design/icons';
 import { Tooltip } from 'antd';
+import useUser from './../../hooks/useUser';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+
+  const user = useUser();
+  const navigator = useNavigate();
+
+  if(!user){
+    return <Navigate to={'/login'}  />
+  }
+
   return (
     <main className={styles.body}>
       <Statistics />
       <Table />
       <Tooltip title={<span>Add New Record</span>}>
-      <button className={styles.addButton}>
+      <button className={styles.addButton} onClick={() => navigator('/new')}>
         <PlusOutlined />
       </button>
       </Tooltip>
