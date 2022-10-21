054a876c7e00a78ea09c9464275afd23d5a3e050
Blanco237 <asongrandy9@gmail.com>
2022-10-21 12:29:36 +0100

    destructuring user

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-import styles from './dash.module.css';
+import styles from "./dash.module.css";
 
-
-import Statistics from '../../components/dashboard/statistics/Statistics'
-import Table from '../../components/dashboard/table/Table'
-import { PlusOutlined } from '@ant-design/icons';
-import { Tooltip } from 'antd';
-import useUser from './../../hooks/useUser';
-import { Navigate, useNavigate } from 'react-router-dom';
+import Statistics from "../../components/dashboard/statistics/Statistics";
+import Table from "../../components/dashboard/table/Table";
+import { PlusOutlined } from "@ant-design/icons";
+import { Tooltip } from "antd";
+import useUser from "./../../hooks/useUser";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-
-  const user = useUser();
+  const { user } = useUser();
   const navigator = useNavigate();
 
-  if(!user){
-    return <Navigate to={'/login'}  />
+  if (!user) {
+    return <Navigate to={"/login"} />;
   }
 
   return (
@@ -24,12 +22,12 @@ const Dashboard = () => {
       <Statistics />
       <Table />
       <Tooltip title={<span>Add New Record</span>}>
-      <button className={styles.addButton} onClick={() => navigator('/new')}>
-        <PlusOutlined />
-      </button>
+        <button className={styles.addButton} onClick={() => navigator("/new")}>
+          <PlusOutlined />
+        </button>
       </Tooltip>
     </main>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
