287bd901e18c0ce800a83035d5bce2158ef246be
Blanco237 <asongrandy9@gmail.com>
2022-10-21 09:07:28 +0100

    Setup Logout Functionality

--- client/src/components/shared/header/Header.jsx
+++ client/src/components/shared/header/Header.jsx
@@ -1,20 +1,33 @@
-import { LogoutOutlined } from '@ant-design/icons'
-import React from 'react'
+import { LogoutOutlined } from "@ant-design/icons";
+import React from "react";
 
-import styles from './header.module.css'
+import styles from "./header.module.css";
+import useUser from "./../../../hooks/useUser";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const { user, handleUser } = useUser();
+  const navigator = useNavigate();
+
+  const logOut = async () => {
+    localStorage.removeItem('drH-user-token');
+    handleUser(null);
+    navigator('/login');
+  }
+
   return (
     <header className={styles.body}>
-        <section className={styles.headline}>
-            DrNG | PATIENTS
-        </section>
-        <button>
-          <span>Logout</span> 
-          <span><LogoutOutlined /></span>
+      <section className={styles.headline}>DrNG | PATIENTS</section>
+      {user && (
+        <button onClick={logOut}>
+          <span>Logout</span>
+          <span>
+            <LogoutOutlined />
+          </span>
         </button>
+      )}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
