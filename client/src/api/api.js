ff93670e6047fde14c56c9f793224ab840c4526e
Blanco237 <asongrandy9@gmail.com>
2022-10-21 23:47:51 +0100

    Modified api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const Axios = axios.create({
     baseURL: 'http://localhost:5500',
-    headers: {
-        token: localStorage.getItem('drH-user-token')
-    }
 });
 
 export const loginUser = async (user) => {
@@ -28,27 +25,47 @@ export const getUser = async (token) => {
 
 
 export const saveRecord = async (data) => {
-    const res = await Axios.post('/records/save', data);
+    const res = await Axios.post('/records/save', data, {
+        headers: {
+            token: localStorage.getItem('drH-user-token')
+        }
+    });
 
     return res.data
 }
 
 export const getRecords = async () => {
-    const res = await Axios.get('/records/all');
+    const res = await Axios.get('/records/all', {
+        headers: {
+            token: localStorage.getItem('drH-user-token')
+        }
+     } );
     return res.data;
 }
 
 export const getRecord = async (code) => {
-    const res = await Axios.get(`/records/byCode/${code}`);
+    const res = await Axios.get(`/records/byCode/${code}`, {
+        headers: {
+            token: localStorage.getItem('drH-user-token')
+        }
+     } );
     return res.data;
 }
 
 export const updateRecord = async (data) => {
-    const res = await Axios.post('/records/update', data);
+    const res = await Axios.post('/records/update', data, {
+        headers: {
+            token: localStorage.getItem('drH-user-token')
+        }
+     } );
     return res.data;
 }
 
 export const getStatusCount = async (status) => {
-    const res = await Axios.get(`/records/status/${status}`);
+    const res = await Axios.get(`/records/status/${status}`, {
+        headers: {
+            token: localStorage.getItem('drH-user-token')
+        }
+     } );
     return res.data;
 }
