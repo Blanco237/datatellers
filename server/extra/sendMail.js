fdc998fe8278ae8c16e2e532f2da1d204dd4a6a2
Blanco237 <asongrandy9@gmail.com>
2022-10-21 21:36:26 +0100

    Created Send Mail function

---
+++ server/extra/sendMail.js
@@ -0,0 +1,53 @@
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const path = require('path');
+require('dotenv').config();
+
+
+const sendMail = (data) => {
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.USER_EMAIL,
+            pass: process.env.USER_PASS
+        }
+    });
+
+    const handlebarOptions = {
+        viewEngine: {
+            extName : ".handlebars",
+            partialsDir: path.resolve('./templates'),
+            defaultLayout: false,
+        },
+        viewPath: path.resolve('./templates'),
+        extName: ".handlebars"
+    }
+
+    transporter.use("compile", hbs(handlebarOptions));
+
+    const mailOptions = {
+        to: 'asongrandy9@gmail.com',
+        subject: "New Appointment",
+        template: "email",
+        context: {
+            code: data.code,
+            name: data.name,
+            status: data.status,
+            appt: data.appt,
+            time: data.time,
+            link: "google.com"
+        }
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err){
+            console.error(err);
+        }
+        else{
+            console.log("Email sent: " + info.response);
+        }
+    })
+}
+
+module.exports = sendMail;
