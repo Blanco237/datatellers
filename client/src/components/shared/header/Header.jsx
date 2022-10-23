e0193fc007f473e962527340b08ee77bcbb85689
Blanco237 <asongrandy9@gmail.com>
2022-10-23 17:53:11 +0100

    Added Functionality for add user modal

--- client/src/components/shared/header/Header.jsx
+++ client/src/components/shared/header/Header.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import useUser from "./../../../hooks/useUser";
 
 import styles from "./header.module.css";
-import { LogoutOutlined } from "@ant-design/icons";
+import { LogoutOutlined, UserAddOutlined } from "@ant-design/icons";
+import Register from "../modals/Register";
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   const { user, handleUser } = useUser();
   const navigator = useNavigate();
 
@@ -17,17 +19,27 @@ const Header = () => {
   };
 
   return (
+    <>
     <header className={styles.body}>
       <section className={styles.headline}>DrNG | PATIENTS</section>
       {user && (
-        <button onClick={logOut}>
+        <>
+        <button onClick={logOut} className={styles.logOut}>
           <span>Logout</span>
           <span>
             <LogoutOutlined />
           </span>
         </button>
+        <button className={styles.add} onClick={() => setShowModal(true)} >
+          <UserAddOutlined />
+        </button>
+        </>
       )}
     </header>
+    {
+      showModal && <Register closeModal={() => setShowModal(false)} />
+    }
+    </>
   );
 };
 
